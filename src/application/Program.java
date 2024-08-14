9221cb76cde6b4868c2f165c8ef3c6d5005d024b
Leonardo Marinho <leowalkg6@gmail.com>
2024-08-13 23:08:28 -0300

    fix the syntax and logical

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
 			System.out.println("Pagador #"+index+" dados:");
 			System.out.println("Pessoa Fisica ou Pessoa Juridica? pf/pj ");
 			String option = input.next();
-			Pessoa pagador;
+			Pessoa pagador = null;
 			
 			if(option.equals("pf")) {
 				pagador = new PessoaFisica();
@@ -30,15 +30,27 @@ public class Program {
 			else if(option.equals("pj")){
 				pagador = new PessoaJuridica();
 			}
-			System.out.println("Opção inválida, ignorando este pagador.");
-            continue; // pula para o próximo ciclo do loop
+			//System.out.println("Opção inválida, ignorando este pagador.");
+            //continue; // pula para o próximo ciclo do loop
+            
+            String nome;
+    		double gastosAnuais=0;
+    		System.out.println("Digite o nome do pagador:");
+    		nome=input.nextLine();
+    		gastosAnuais=input.nextDouble();
+    		
+    		pagador.setNome(nome);
+    		if(pagador instanceof PessoaFisica){
+    			double gastosSaude = 0;
+    			((PessoaFisica) pagador).setGastosSaude(gastosSaude);
+    		}
+    		else if (pagador instanceof PessoaJuridica){
+    			int quantFunc = 0;
+    			((PessoaJuridica) pagador).setNumFuncionario(quantFunc);
+    		}
 		}
 		
-		
-		
-		
-		
-
+		input.close();
 	}
 
 }
